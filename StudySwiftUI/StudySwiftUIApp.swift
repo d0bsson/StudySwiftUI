62ae0b22be6ca423b3f3de50a9b879f228149de3
David <ber.dev@yandex.ru>
2021-06-01 22:05:00 +0300

    Initial Commit

---
+++ StudySwiftUI/StudySwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  StudySwiftUIApp.swift
+//  StudySwiftUI
+//
+//  Created by Дэвид Бердников on 01.06.2021.
+//
+
+import SwiftUI
+
+@main
+struct StudySwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
